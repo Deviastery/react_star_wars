f3f6db95af44ea9a9a6e9846b6539d3564311fec
Viktoria T <deviastery@gmail.com>
2024-01-26 12:22:48 +0300

    Not Found page is added

---
+++ src/containers/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import { useLocation } from "react-router-dom";
+import img from './img/not_found.jpg';
+import styles from './NotFoundPage.module.css';
+
+const NotFoundPage = () => {
+    let location = useLocation();
+    return (
+        <>
+            <img className={styles.img} src={img} alt="Not found" />
+            <p className={styles.text}>No match for <u>{location.pathname}</u></p>
+        </>
+    )
+}  
+
+export default NotFoundPage;
