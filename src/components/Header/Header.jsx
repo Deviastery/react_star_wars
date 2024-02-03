468d78cba3584981c68c57bfd997b950e22500b0
Viktoria T <deviastery@gmail.com>
2024-02-03 23:38:19 +0300

    feature(HomePage): add theme switcher and styles in ChooseSide

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useTheme, THEME_LIGHT, THEME_DARK, THEME_NEITRAL } from '@context/ThemeProvider';
 import Favorite from '@components/Favorite';
+
+import imgR2D2 from './img/r2d2.svg';
+import imgChubaka from './img/chubaka.svg';
+import imgYoda from './img/yoda.svg';
 
 import styles from './Header.module.css';
 
 const Header = () => {
+    const [icon, setIcon] = useState(imgYoda);
+    const isTheme = useTheme();
+
+    useEffect(() => {
+        switch (isTheme.theme) {
+            case THEME_LIGHT: setIcon(imgR2D2); break;
+            case THEME_DARK: setIcon(imgChubaka); break;
+            case THEME_NEITRAL: setIcon(imgYoda); break;
+            default: setIcon(imgYoda);
+        }
+    }, [isTheme])
+
     return (
         <div className={styles.container}>
+            <img className={styles.logo} src={icon} alt="Star Wars" />
             <ul className={styles.list__container}>
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="/people/?page=1">People</NavLink></li>
