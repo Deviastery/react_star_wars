02b6dc2e08b602b88dc6ebb3967976b3b7c4e779
Viktoria T <deviastery@gmail.com>
2024-02-04 16:17:32 +0300

    feature(SearchPage): add character searching

---
+++ src/containers/SearchPage/SearchPage.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+import { withErrorApi } from '@hoc-helpers/withErrorApi';
+import { getApiResource } from '@utils/network';
+import { API_SEARCH } from '@constants/api';
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData';
+
+import SearchPageInfo from '@components/SearchPage/SearchPageInfo';
+
+import styles from './SearchPage.module.css';
+
+const SearchPage = ({ setErrorApi }) => {
+    const [inputSearchValue, setInputSearchValue] = useState('');
+    const [people, setPeople] = useState([]);
+
+    const getResponse = async param => {
+        const res = await getApiResource(API_SEARCH+param);
+
+        if (res) {
+            const peopleList = res.results.map(({ name, url }) => {
+                const id = getPeopleId(url);
+                const img = getPeopleImage(id);
+
+                return {
+                    id,
+                    name, 
+                    img
+                }
+            });
+            
+            setPeople(peopleList);
+            setErrorApi(false);
+        } else {
+            setErrorApi(true);
+        }
+    }
+
+    useEffect(() => {
+        getResponse('');
+    }, []);
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+
+        setInputSearchValue(value);
+        getResponse(value);
+    }
+
+    return (
+        <>
+            <h1 className="header__text">Search</h1>
+            <input 
+                type="text" 
+                value={inputSearchValue}
+                onChange={handleInputChange}
+                placeholder="Input character's name"
+            />
+            <SearchPageInfo people={people} />
+        </>
+    )
+}
+
+SearchPage.propTypes = {
+    SetErrorApi: PropTypes.func
+}
+
+export default withErrorApi(SearchPage);
